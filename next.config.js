2831f1ed98fd2f4b4437354f1f42a05701802bec
nagarmoshe <nagarmoshe@microsoft.com>
2023-10-08 19:09:35 +0300

    init nextjs with map
    Co-authored-by: Shai <shayy9999@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'node_modules/leaflet/dist/images',
+            to: path.resolve(__dirname, 'public', 'leaflet', 'images')
+          },
+        ],
+      }),
+    )
+    return config
+  }
+}
+
+module.exports = nextConfig;
